e97415d1ad0cfdc42b1392efb8fba0e9b031f9a6
kimwan14 <eung990@naver.com>
2023-06-06 22:42:23 +0900

    10 commit

--- src/main/java/com/co/kr/controller/MBTIListController.java
+++ src/main/java/com/co/kr/controller/MBTIListController.java
@@ -9,21 +9,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.co.kr.domain.BoardFileDomain;
-import com.co.kr.domain.BoardListDomain;
 import com.co.kr.domain.MBTIFileDomain;
 import com.co.kr.domain.MBTIListDomain;
 import com.co.kr.service.MBTIUploadService;
-import com.co.kr.vo.FileListVO;
 import com.co.kr.vo.MBTIListVO;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
 public class MBTIListController {
 
 	@Autowired
